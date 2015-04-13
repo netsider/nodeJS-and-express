732e06e8bd143d6e9d0107cbdb3005a076b0b85d
Russell Rounds <rdoubleoc@aol.com>
2015-04-13 03:39:24 -0400

    commit: 15
    got zipfile working

---
+++ zipfile/index.js
@@ -0,0 +1,27 @@
+	var AdmZip = require('adm-zip');
+
+	// reading archives
+	var zip = new AdmZip("./zipfile.zip");
+	var zipEntries = zip.getEntries(); // an array of ZipEntry records
+    zipEntries.forEach(function(zipEntry) {
+        console.log(zipEntry.toString()); // outputs zip entries information
+        if (zipEntry.entryName == "my_file.txt") {
+             console.log(zipEntry.data.toString('utf8')); 
+        }
+    });
+    // outputs the content of some_folder/my_file.txt
+    console.log(zip.readAsText("output.txt")); 
+    // extracts the specified file to the specified location
+    zip.extractEntryTo(/*entry name*/"some_folder/my_file.txt", /*target path*/"./home/", /*maintainEntryPath*/false, /*overwrite*/true);
+    // extracts everything
+    zip.extractAllTo(/*target path*/"./home", /*overwrite*/true);
+    // creating archives
+    var zip = new AdmZip();
+    // add file directly
+    zip.addFile("test.txt", new Buffer("inner content of the file"), "entry comment goes here");
+    // add local file
+    zip.addLocalFile("test.bmp");
+    // get everything as a buffer
+    var willSendthis = zip.toBuffer();
+    // or write everything to disk
+    zip.writeZip(/*target file name*/"newfile.zip");
