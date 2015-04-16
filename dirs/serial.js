a3d693a36c747faca4ac370f7aaa537b850d72ec
Russell Rounds <rdoubleoc@aol.com>
2015-04-15 23:24:16 -0400

    Commit: 21

--- dirs/serial.js
+++ dirs/serial.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var md5 = require('md5');
 var count = 1;
 var Dirs = [];
 var fileNames = [];
@@ -6,7 +7,8 @@ var results = [];
 var DIR = 'C:/xampp/htdocs';
 var SEP = ':';
 var printToScreen = false;
-// var printToScreen = true;
+var currmd5;
+var hashObject = {};
 var walk = function(dir, done) {
 	fs.readdir(dir, function(err, list) {
 		if (err) return done(err);
@@ -18,22 +20,25 @@ var walk = function(dir, done) {
 			file = dir + '/' + file;
 			fs.stat(file, function(err, stat) {
 				if (stat && stat.isDirectory()) { // If item is a directory
-				if(printToScreen){
-					process.stdout.write(count + '.' + filename + '\r'); //  print and number each file
+				if(printToScreen){ // Toggle on/off "!" to print to screen
+					process.stdout.write(count + '.' + filename + '\r'); //  print and number each directory
 					count++;
 				}
 				Dirs.push(filename);
-				
 				walk(file, function(err, res) {
 					// results = results.concat(res); // No idea (?)
 					next();
 				});
-        } else {
-			
+        } else { // if item is a file
 			results.push(file);
 			fileNames.push(filename);
-			if(!printToScreen){
-				process.stdout.write(count + '.' + filename + '\r'); //  print and number each file
+			hashObject[count] = {};
+			var currmd5 = md5(file);
+			hashObject[count].name = filename;
+			hashObject[count].hash = currmd5;
+			if(!printToScreen){ // Toggle on/off "!" to print to screen
+				//process.stdout.write(count + '.' + filename + '\r'); //  print and number each file
+				process.stdout.write(count + '.' + filename + ' (MD5: ' + currmd5 + ') ' + '\r'); //  print and number each file
 				count++;
 			}
 			next();
@@ -51,6 +56,7 @@ var doneFunction = function(err, list){
 	if(list){
 		console.log('Number of files in ' + DIR + SEP + ' ' + fileNames.length);
 		console.log('Number of directories in ' + DIR + SEP + ' ' + Dirs.length);
+		console.log(JSON.stringify(hashObject));
 	}
 };
 walk(DIR, doneFunction);
